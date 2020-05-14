27e5a5781061cd01d2f76e323b7e6d1911d0d9fa
Rafael Timbó <timbo.rafa@gmail.com>
2020-05-14 18:33:56 -0300

    more card tests

--- src/app/components/Card.test.js
+++ src/app/components/Card.test.js
@@ -24,7 +24,39 @@ it("renders empty Card", () => {
   expect(container).toBeEmpty();
 });
 
-it("renders Card", () => {
+it("renders Card's title", () => {
+  const title = "Test Title";
+  act(() => {
+    render(<Card title={title} ></Card>, container);
+  });
+  expect(container.textContent).toEqual(title);
+});
+
+it("renders Card's primary text", () => {
+  const primaryText = "Test primary text";
+  act(() => {
+    render(<Card primaryText={primaryText}></Card>, container);
+  });
+  expect(container.textContent).toContain(primaryText);
+});
+
+it("renders Card's secondary text", () => {
+  const secondaryText = "Test secondary text";
+  act(() => {
+    render(<Card secondaryText={secondaryText} ></Card>, container);
+  });
+  expect(container.textContent).toContain(secondaryText);
+});
+
+it("renders Card's secondary text", () => {
+  const buttonText = "Test button text";
+  act(() => {
+    render(<Card secondaryText={buttonText} ></Card>, container);
+  });
+  expect(container.textContent).toContain(buttonText);
+});
+
+it("renders complete Card", () => {
   const title = "Test Title";
   const primaryText = "Test primary text";
   const secondaryText = "Test secondary text";
